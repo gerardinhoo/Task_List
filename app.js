7e75f22ed6b64c077f59575b52b22afe856dc3d2
Gerard Eklu <GXE6285@c02w709fhtd6mbp.amer.homedepot.com>
2018-11-19 08:54:17 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+// Define UI Variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear Task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Add task function
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class;
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+
+  // Append the link to li
+  li.appendChild(link);
+
+  // append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Delete this item?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear tasks
+function clearTasks() {
+  // One way to clear tasks
+  taskList.innerHTML = '';  
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach((task) => {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
